7d707e17c1493aa95351bf92b97b6ac230aefc6c
Sankalpservices <sankalpservices2025@gmail.com>
2025-09-25 23:46:23 +0530

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/societydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connected');
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend working');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
